6010f55c5637254821c33202e93fa93dae19c16b
Vladimir Kobzev <93667972+Kobzev0826@users.noreply.github.com>
2023-01-18 15:55:16 +0300

    Create hack_scripts.py

---
+++ hack_scripts.py
@@ -0,0 +1,82 @@
+from datacenter.models import (
+    Chastisement, Schoolkid, Mark, Lesson, Commendation, Subject)
+from sys import exit
+import random
+
+
+def get_schoolkid_info(child_full_name):
+    try:
+        child_info = Schoolkid.objects.get(full_name__contains=child_full_name)
+    except Schoolkid.MultipleObjectsReturned:
+        exit('ОШИБКА: Найдено несколько учеников')
+    except Schoolkid.DoesNotExist:
+        exit('ОШИБКА: имя не найдено.')
+    return child_info
+
+
+def get_lesson(lesson,kid):
+    lessons_info = Lesson.objects.filter(
+        group_letter=kid.group_letter,
+        year_of_study=kid.year_of_study,
+        subject__title=lesson).order_by('-date').first()
+    if lessons_info is None:
+        exit("ОШИБКА: Неверно введено название предмета")
+    return lessons_info
+
+
+def change_bad_marks(schoolkid):
+    kid = get_schoolkid_info(schoolkid)
+    all_bad_marks = Mark.objects.filter(
+        schoolkid=kid, points__in=[1, 2, 3])
+    for bad_mark in all_bad_marks:
+        bad_mark.points = 5
+        bad_mark.save()
+
+
+def remove_chastisements(schoolkid):
+    all_bad_marks = Chastisement.objects.filter(schoolkid=get_schoolkid_info(schoolkid))
+    for bad_mark in all_bad_marks:
+        bad_mark.delete()
+
+
+def create_commendation(schoolkid,subject):
+
+    kid = get_schoolkid_info(schoolkid)
+    lesson = get_lesson(subject,kid)
+    praise_list = ["Молодец!",
+                   "Отлично!",
+                   "Хорошо!",
+                   "Гораздо лучше, чем я ожидал!",
+                   "Ты меня приятно удивил!",
+                   "Великолепно!",
+                   "Прекрасно!",
+                   "Ты меня очень обрадовал!",
+                   "Именно этого я давно ждал от тебя!",
+                   "Сказано здорово – просто и ясно!",
+                   "Ты, как всегда, точен!",
+                   "Очень хороший ответ!",
+                   "Талантливо!",
+                   "Ты сегодня прыгнул выше головы!",
+                   "Я поражен!",
+                   "Уже существенно лучше!",
+                   "Потрясающе!",
+                   "Замечательно!",
+                   "Прекрасное начало!",
+                   "Так держать!",
+                   "Ты на верном пути!",
+                   "Здорово!",
+                   "Это как раз то, что нужно!",
+                   "Я тобой горжусь!",
+                   "С каждым разом у тебя получается всё лучше!",
+                   "Мы с тобой не зря поработали!",
+                   "Я вижу, как ты стараешься!",
+                   "Ты растешь над собой!",
+                   "Ты многое сделал, я это вижу!",
+                   "Теперь у тебя точно все получится!"
+                   ]
+    Commendation.objects.create(
+        teacher=lesson.teacher,
+        subject=lesson.subject,
+        created=lesson.date,
+        schoolkid=kid,
+        text=random.choice(praise_list))
